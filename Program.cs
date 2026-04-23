835fbe6005ccbf445f6add96fe5346fb81845b00
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2026-04-23 22:47:03 +0530

    Get stocks

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));    
+});
 
 var app = builder.Build();
 
@@ -21,6 +30,7 @@ app.UseHttpsRedirection();
 //test endpoint
 app.MapGet("/", () =>"API is running!");
 
+app.MapControllers();  //for swagger to work
 
 app.Run();
 
--- obj/Debug/net10.0/api.AssemblyInfo.cs
+++ obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e458332beef27f9737c04646cafc16f6564880dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04dd057d96fe25d8ea0ae0834cf6e24eb36515ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

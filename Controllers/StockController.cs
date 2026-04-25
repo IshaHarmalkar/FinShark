8f0c044e269c8251c6a891d5bd2b4012ca84149c
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2026-04-25 14:02:36 +0530

    Added validation rules to comment and stock

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -30,6 +30,9 @@ namespace api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var stocks = await _stockRepo.GetAllAsync();
 
             var stockDto = stocks.Select(s => s.ToStockDto());
@@ -38,9 +41,11 @@ namespace api.Controllers
         }
 
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
             var stock = await _stockRepo.GetByIdAsync(id);       
 
             if(stock == null)
@@ -54,6 +59,8 @@ namespace api.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
             var stockModel = stockDto.ToStockFromCreateDto();
             await _stockRepo.CreateAsync(stockModel);
            
@@ -62,9 +69,11 @@ namespace api.Controllers
 
 
         [HttpPut]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
         {       
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
             var stockModel = await  _stockRepo.UpdateAsync(id, updateDto);
 
             if (stockModel == null)
@@ -79,9 +88,11 @@ namespace api.Controllers
         }
 
         [HttpDelete]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Delete([FromRoute] int id )
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
             var stockModel = await _stockRepo.DeleteAsync(id);
             if(stockModel == null)
             {
